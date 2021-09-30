27d4a66f8c9bb3001452249aac4b7b20eaeb026c
unknown <knitish123@gmail.com>
2021-09-30 13:32:27 +0530

    dB connection string and return types of function changes

--- __init__.py
+++ __init__.py
@@ -5,6 +5,9 @@ from asteval import Interpreter
 from config_reader import config_reader
 from elasticsearch import helpers
 
+
+
+
 aeval = Interpreter()
 
 class Migrator():
@@ -14,7 +17,6 @@ class Migrator():
 		self.com_cur = self.com_connection()
 		self.indx_name = self.output['EXTRACTION']['INDEX']
 		self.indx_size_es = self.find_stop_point_es() #To be transferred
-		
 		print (self.indx_size_es)
 		try:
 			self.mongo_mark=self.find_resume_point_mongo()
@@ -33,8 +35,9 @@ class Migrator():
 
 		self.mark = self.es_mark
 		
+		
 		if self.indx_size_es == 0:
-			print ('Transfer Complete!!!')
+			print ('Transfer Completed!!!')
 			return None
 
 		while self.stop_mark != 0:
@@ -44,7 +47,7 @@ class Migrator():
 			self.stop_mark = self.find_stop_point_es()
 			print ('%s - Partition transfer completed'%(str(self.mark)))
 			print ('%s - documents left'%(str(self.stop_mark)))
-		print ('Transfer Complete!!!')
+		print ('Transfer Completed!!!')
 		return None
 
 	def ext_connection(self):
@@ -60,15 +63,8 @@ class Migrator():
 	def com_connection(self):
 		try:
 			database = self.output['COMMIT']
-			try:
-				if database['USER'] != '' and database['USER'] != None:
-					uri = "mongodb://%s:%s@%s:%s" % (database['USER'], database['PASSWORD'], database['HOST'], database['PORT'])
-				else:
-					uri = "mongodb://%s:%s" % (database['HOST'], database['PORT'])
-			except:
-				uri = "mongodb://localhost:27017"
-			client = MongoClient(uri)
-			cur = client[database['DATABASE']]
+			client = MongoClient("mongodb+srv://username:password@maxonic.ui8v9.mongodb.net/dbName?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority")
+			cur = client.test
 			col = cur[database['COLLECTION']]
 			return col
 		except Exception as e:
@@ -83,7 +79,6 @@ class Migrator():
 			self.no_trace(hits_)
 			return {"status": "succeeded"}
 		except Exception as e:
-			print (e)
 			hits_ = []
 		return hits_
 
@@ -128,6 +123,7 @@ class Migrator():
 				print ('copied')
 			return trace_
 		except Exception as e:
+			print(e)
 			return {}
 
 	def no_trace(self, list_):
@@ -160,33 +156,39 @@ class Migrator():
 
 	def find_resume_point_mongo(self):
 		try:
-			return int(self.com_cur.find().sort("_id", DESCENDING).limit(1)[0]["_id"])
+			ans = self.com_cur.find().sort("_id", DESCENDING).limit(1)[0]["_id"]
+			return (self.com_cur.find().sort("_id", DESCENDING).limit(1)[0]["_id"])
 		except Exception as e:
 			return 0
 
 	def find_resume_point_es(self):
 		try:
 			res_point = self.ext_cur.search(index = self.indx_name, size=0, body={"aggs" : {"max_mongoes_id" : { "max" : { "field" : "mongoes_id" }}}})
-			return int(res_point['aggregations']['max_mongoes_id']['value'])
+			if res_point == None:
+				return 0
+			else:
+				return int(res_point['aggregations']['max_mongoes_id']['value'])
 		except Exception as e:
 			return 0
 
 	def find_index_size_es(self):	#to_be_migrated
 		try:
-			return int(self.ext_cur.search(index=self.indx_name, size=0)['hits']['total'])
+			ans2 = self.ext_cur.search(index=self.indx_name, size=0)['hits']['total']
+			return ans2['value']
 		except:
 			return 0
 
 	def find_stop_point_es(self):
 		try:
-			return int(self.ext_cur.search(index = self.indx_name, size=0, body={"query": {"bool": {"must_not": {"exists": {"field": "mongoes_id"}}}}})['hits']['total'])
+			ans3 = self.ext_cur.search(index = self.indx_name, size=0, body={"query": {"bool": {"must_not": {"exists": {"field": "mongoes_id"}}}}})['hits']['total']
+			return ans3['value']
 		except Exception as e:
-			print (e)
 			return 0
 
 	def mongoes_id_exists(self):
 		try:
-			return int(self.ext_cur.search(index=self.indx_name, size=0, body={"query": {"exists" : { "field" : "mongoes_id" }}})['hits']['total'])
+			ans4 = self.ext_cur.search(index=self.indx_name, size=0, body={"query": {"exists" : { "field" : "mongoes_id" }}})['hits']['total']
+			return ans4['value']
 		except:
 			return 0
 
